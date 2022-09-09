3456d3992ba2c469906b236ee2adfc2aa2ada5a3
pattisonsam <pattisonsam@urbandaleschools.com>
2022-09-09 09:00:52 -0500

    done with ch.02

---
+++ Python Practice Log.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print(10.1//2) # floor division
+print(9%2) # the remainder
+print(abs(-7)) # absolute value
+print()
+a,b=(5,3)
+print(a)
+print(b)
+x=1
+print(x)
+x+=1 # same as x=x+1
+print(x)
+
+y= 3*(a+b)
+print(y)
+
+x,y,z = (3,4,5)
+ave= (x+y+z)/3
+print(ave)
